ba0bf919963fc6e825bf731994b95afe33233b97
Wayne Zheng <wnz6klj@gmail.com>
2021-04-03 17:44:33 -0400

    extracting data from major news networks

--- dataCollecting.py
+++ dataCollecting.py
@@ -1,18 +1,83 @@
 import newspaper
+import pandas as pd
 
-cnn_paper = newspaper.build('http://cnn.com')
+homeUrlYahoo = "https://news.yahoo.com/"
+homeUrlNYtimes = "https://www.nytimes.com/"
+homeUrlWallSt = "https://www.wsj.com/"
+homeUrlWaPost = "https://www.washingtonpost.com/"
 
-urlList = []
-for article in cnn_paper.articles:
-    print(article.url)
-    urlList.append(article.url)
+yahoo = newspaper.build(homeUrlYahoo, language='en', memoize_articles=False)
+nyTimes = newspaper.build(homeUrlNYtimes, language='en', memoize_articles=False)
+wallSt = newspaper.build(homeUrlWallSt, language='en', memoize_articles=False)
+waPost = newspaper.build(homeUrlWaPost, language='en', memoize_articles=False)
 
-for category in cnn_paper.category_urls():
-    print(category)
+news_title = []
+news_text = []
+news_date =[]
 
-for url in urlList:
-    a = newspaper.Article(url, language='en')
-    a.download()
-    a.parse()
-    print(a.title)
-    print(a.text[:150])
+news = yahoo.articles
+
+for i in range(len(news)):
+    paper = news[i]
+    try:
+        paper.download()
+        paper.parse()
+        news_title.append(paper.title)
+        news_text.append(paper.text)
+        news_date.append(paper.publish_date)
+    except:
+        news_title.append('NULL')
+        news_text.append('NULL')
+        news_date.append('NULL')
+        continue
+
+news = nyTimes.articles
+
+for i in range(len(news)):
+    paper = news[i]
+    try:
+        paper.download()
+        paper.parse()
+        news_title.append(paper.title)
+        news_text.append(paper.text)
+        news_date.append(paper.publish_date)
+    except:
+        news_title.append('NULL')
+        news_text.append('NULL')
+        news_date.append('NULL')
+        continue
+
+news = wallSt.articles
+
+for i in range(len(news)):
+    paper = news[i]
+    try:
+        paper.download()
+        paper.parse()
+        news_title.append(paper.title)
+        news_text.append(paper.text)
+        news_date.append(paper.publish_date)
+    except:
+        news_title.append('NULL')
+        news_text.append('NULL')
+        news_date.append('NULL')
+        continue
+
+news = waPost.articles
+
+for i in range(len(news)):
+    paper = news[i]
+    try:
+        paper.download()
+        paper.parse()
+        news_title.append(paper.title)
+        news_text.append(paper.text)
+        news_date.append(paper.publish_date)
+    except:
+        news_title.append('NULL')
+        news_text.append('NULL')
+        news_date.append('NULL')
+        continue
+
+data = pd.DataFrame({'title': news_title, 'text':news_text, 'data':news_date})
+data.to_csv('extracted.csv', index=False, sep=',')
